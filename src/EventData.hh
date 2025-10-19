70355ca89a874cc53b9eb38a0a10f09e43c770b2
John Ho <johnho553@tamu.edu>
2025-10-19 02:12:12 -0500

    Add simple event data class for processing event data and exposing as vector. Need to test

--- src/EventData.hh
+++ src/EventData.hh
@@ -3,8 +3,150 @@
 #ifndef EVENTDATA_HH
 #define EVENTDATA_HH
 
+#include <vector>
+#include <deque>
+#include <glm/glm.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <set>
+
+
 class EventData
 {
+	// Represents single event data
+	struct EventDatum
+	{
+            int32_t x;
+            int32_t y;
+            int64_t timestamp;
+            uint8_t polarity;
+	};
+
+	struct FrameDatum
+	{
+            cv::Mat frameData;
+            int64_t timestamp;
+	};
+
+	public:
+
+		/**
+		 * @brief Default constructor for event data.
+		 */
+        EventData() : evt_data{}, frame_data{}
+		{
+
+		}
+
+        /**
+         * @brief Inserts event data into internel ordered set of event data.
+         */
+        void write_evt_data(EventDatum raw_evt_data)
+        {
+            evt_data.insert(raw_evt_data);
+        }
+
+        /**
+         * @brief Inserts frame data into internal ordered set of frame data.
+         */
+        void write_frame_data(FrameDatum raw_frame_data)
+        {
+            frame_data.insert(raw_frame_data);
+
+        }
+
+        /**
+         * @brief Exposes event data with absolute timestamp as a vector of glm::vec4.
+         */
+        std::vector<glm::vec4> get_evt_vector_absolute()
+        {
+            std::vector<glm::vec4> evt_vector{};
+            cull_elements(evt_vector, evt_data, 0.8f, 0.5f);
+            for (EventDatum evt_el : evt_data)
+            {
+                float x{static_cast<float>(evt_el.x)};
+                float y{static_cast<float>(evt_el.y)};
+                float timestamp{static_cast<float>(evt_el.timestamp)};
+                float polarity{static_cast<float>(evt_el.polarity)};
+                evt_vector.push_back(glm::vec4{x, y, timestamp, polarity});
+            }
+
+            return evt_vector;
+        }
+
+        /**
+         * @brief Exposes event data with relative timestamp as a vector of glm::vec4.
+         */
+        std::vector<glm::vec4> get_evt_vector_relative()
+        {
+            std::vector<glm::vec4> evt_vector{};
+            cull_elements(evt_vector, evt_data, 0.8f, 0.5f);
+            for (EventDatum evt_el : evt_data)
+            {
+                float x{static_cast<float>(evt_el.x)};
+                float y{static_cast<float>(evt_el.y)};
+                float timestamp{static_cast<float>(evt_el.timestamp - (*evt_data.begin()).timestamp)}; // Relative timestamp
+                float polarity{static_cast<float>(evt_el.polarity)};
+                evt_vector.push_back(glm::vec4{x, y, timestamp, polarity});
+            }
+
+            return evt_vector;
+        }
+
+        /**
+         * @brief Exposes frame data with absolute timestamp as a vector of pairs containing image data and timestamp. 
+         */
+        std::vector<std::pair<cv::Mat, float>> get_frame_vector_absolute()
+        {
+            std::vector<std::pair<cv::Mat, float>> frame_vector{};
+            cull_elements(frame_vector, frame_data, 0.8f, 0.5f);
+            for (FrameDatum frame_el : frame_data)
+            {
+                float timestamp{static_cast<float>(frame_el.timestamp)};
+                frame_vector.push_back(std::make_pair(frame_el.frameData, timestamp));
+            }
+
+            return frame_vector;
+        }
+
+        /**
+         * @brief Exposes frame data with relative timestamp as a vector of pairs containing image data and timestamp.
+         */
+        std::vector<std::pair<cv::Mat, float>> get_frame_vector_relative()
+        {
+            std::vector<std::pair<cv::Mat, float>> frame_vector{};
+            cull_elements(frame_vector, frame_data, 0.8f, 0.5f);
+            for (FrameDatum frame_el : frame_data)
+            {
+                float timestamp{static_cast<float>(frame_el.timestamp - (*frame_data.begin()).timestamp)};
+                frame_vector.push_back(std::make_pair(frame_el.frameData, timestamp));
+            }
+
+            return frame_vector;
+        }
+
+	private:
+		std::multiset<EventDatum> evt_data; // Ensures ordered event data
+
+		std::multiset<FrameDatum> frame_data; // Ensures ordered frame data
+
+		/**
+		  * @brief Memory management code.
+		  */
+		template<typename T, typename V>
+		void cull_elements(std::vector<T> &vector_data, std::multiset<V> &data, float max_percentage, float cull_percentage)
+		{
+            size_t maxElements{std::min(vector_data.max_size(), data.max_size())};
+            if (data.size() >= static_cast<size_t>(max_percentage * maxElements)) // If there are more than 90% max number of events
+            {
+                // Ensures upper bound is met no matter the condition
+                while (data.size() > static_cast<size_t>(cull_percentage * maxElements))
+                {
+                    data.erase(data.begin(), data.begin() + static_cast<size_t>((max_percentage - cull_percentage) * evtMaxElements)); // Should bring number of elements down to 50% of max elements
+                }
+            }
+		}	
 };
 
 #endif // EVENTDATA_HH
