caf4c0c6c060b65607fd662bf2b47b665eceeb66
Nick <nickalmeter@tamu.edu>
2025-11-03 20:39:39 -0600

    Fix uniform passing for color control

--- src/DigitalCodedExposure.hh
+++ src/DigitalCodedExposure.hh
@@ -114,7 +114,7 @@ class DigitalCodedExposure
             compute_pipeline_info.num_readonly_storage_buffers = 1;
             compute_pipeline_info.num_readwrite_storage_textures = 3;
             compute_pipeline_info.num_readwrite_storage_buffers = 0;
-            compute_pipeline_info.num_uniform_buffers = 1;
+            compute_pipeline_info.num_uniform_buffers = 0;
             compute_pipeline_info.threadcount_x = 1;
             compute_pipeline_info.threadcount_y = 1;
             compute_pipeline_info.threadcount_z = 1;
@@ -131,7 +131,7 @@ class DigitalCodedExposure
             process_compute_pipeline_info.num_readonly_storage_buffers = 0;
             process_compute_pipeline_info.num_readwrite_storage_textures = 3;
             process_compute_pipeline_info.num_readwrite_storage_buffers = 0;
-            process_compute_pipeline_info.num_uniform_buffers = 0;
+            process_compute_pipeline_info.num_uniform_buffers = 1;
             process_compute_pipeline_info.threadcount_x = 1;
             process_compute_pipeline_info.threadcount_y = 1;
             process_compute_pipeline_info.threadcount_z = 1;
