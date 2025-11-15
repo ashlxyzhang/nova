efca4edbb0960366549c88149531a5794aae8f42
John Ho <johnho553@tamu.edu>
2025-11-14 18:40:05 -0600

    Add integration testing

--- testing/integration_tests.cc
+++ testing/integration_tests.cc
@@ -1,9 +1,199 @@
-#include <gtest/gtest.h>
+#include "../src/DataAcquisition.hh"
+#include "../src/DataWriter.hh"
 #include "../src/EventData.hh"
+#include "../src/ParameterStore.hh"
+#include <gtest/gtest.h>
 #include <iostream>
 
-
-TEST(YES, NO)
+// Function to check order of events.
+// Returns index of mismatch or -1 if no mismatch.
+int32_t check_order_events(EventData &evt_data)
 {
-    EXPECT_STRNE("hello", "world");
+    evt_data.lock_data_vectors();
+    const auto &evt_vector{evt_data.get_evt_vector_ref()};
+
+    for (int32_t i{1}; i < evt_vector.size(); ++i)
+    {
+        // Out of order timestamps detected in event data
+        if (evt_vector[i - 1].z > evt_vector[i].z)
+        {
+            return i;
+        }
+    }
+    evt_data.unlock_data_vectors();
+    return -1;
+}
+
+// Function to check order of frames.
+// Returns index of mismatch or -1 if no mismatch.
+int32_t check_order_frames(EventData &evt_data)
+{
+    evt_data.lock_data_vectors();
+
+    const auto &frame_vector{evt_data.get_frame_vector_ref()};
+
+    for (int32_t i{1}; i < frame_vector.size(); ++i)
+    {
+        // Out of order timestamps detected in event data
+        if (frame_vector[i - 1].second > frame_vector[i].second)
+        {
+            return i;
+        }
+    }
+
+    evt_data.unlock_data_vectors();
+    return -1;
+}
+
+// Function to check equality of events.
+// Returns index of mismatch or -1 if no mismatch.
+int32_t check_event_data_equality(EventData &evt_data1, EventData &evt_data2)
+{
+    evt_data1.lock_data_vectors();
+    evt_data2.lock_data_vectors();
+    const auto &evt_vector1{evt_data1.get_evt_vector_ref()};
+    const auto &evt_vector2{evt_data2.get_evt_vector_ref()};
+
+    for (int32_t i{0}; i < evt_vector1.size(); ++i)
+    {
+        if (evt_vector1[i] != evt_vector2[i])
+        {
+            return i;
+        }
+    }
+    evt_data1.unlock_data_vectors();
+    evt_data2.unlock_data_vectors();
+    return -1;
+}
+
+// Function to check equality of frames.
+// Returns index of mismatch or -1 if no mismatch.
+int32_t check_frame_data_equality(EventData &evt_data1, EventData &evt_data2)
+{
+    evt_data1.lock_data_vectors();
+    evt_data2.lock_data_vectors();
+    const auto &frame_vector1{evt_data1.get_frame_vector_ref()};
+    const auto &frame_vector2{evt_data2.get_frame_vector_ref()};
+
+    for (int32_t i{0}; i < frame_vector1.size(); ++i)
+    {
+        // Check equality of cv matrices
+        auto rows{(frame_vector1[i].first).rows};
+        auto cols{(frame_vector1[i].first).cols};
+        bool unequal{false};
+        for (int32_t row{0}; row < rows; ++row)
+        {
+            for (int32_t col{0}; col < cols; ++col)
+            {
+                if (frame_vector1[i].first.at<char>(row, col) != frame_vector2[i].first.at<char>(row, col))
+                {
+                    unequal = true;
+                    break;
+                }
+            }
+            if (unequal)
+            {
+                break;
+            }
+        }
+
+        // Unequal condition met
+        if (unequal || frame_vector1[i].second != frame_vector2[i].second)
+        {
+            return i;
+        }
+    }
+    evt_data1.unlock_data_vectors();
+    evt_data2.unlock_data_vectors();
+    return -1;
+}
+
+TEST(DataAcquisition, reading)
+{
+    DataAcquisition data_acq{};
+    ParameterStore param_store{};
+    EventData evt_data{};
+    DataWriter data_writer{};
+
+    param_store.add("pop_up_err_str", "");
+
+    ASSERT_EQ(data_acq.init_file_reader("../testing/test_data.aedat4", param_store), true)
+        << "Failed to initialize file for reading: " << param_store.get<std::string>("pop_up_err_str");
+
+    while (data_acq.get_batch_evt_data(evt_data, param_store, data_writer, 1.0f) ||
+           data_acq.get_batch_frame_data(evt_data, param_store, data_writer))
+    {
+    }
+
+    // Check order of timestamps, by aedat standard, should be ordered.
+    int32_t evt_ordered{check_order_events(evt_data)};
+    EXPECT_EQ(evt_ordered, -1) << "Events are out of order at: " << evt_ordered;
+    int32_t frame_ordered{check_order_frames(evt_data)};
+    EXPECT_EQ(frame_ordered, -1) << "Frames are out of order at: " << frame_ordered;
+}
+
+TEST(DataWriter, writing)
+{
+    DataAcquisition data_acq{};
+    ParameterStore param_store{};
+    EventData evt_data{};
+    DataWriter data_writer{};
+
+    param_store.add("pop_up_err_str", "");
+
+    // Initialize file for reading
+    ASSERT_EQ(data_acq.init_file_reader("../testing/test_data.aedat4", param_store), true)
+        << "Failed to initialize file for reading: " << param_store.get<std::string>("pop_up_err_str");
+
+    // Initialize data writer to write to test_data_out.aedat4
+    ASSERT_EQ(data_writer.init_data_writer("../testing/test_data_out.aedat4", data_acq.get_camera_event_width(),
+                                           data_acq.get_camera_event_height(), data_acq.get_camera_frame_width(),
+                                           data_acq.get_camera_frame_height(), true, true, param_store),
+              true)
+        << "Failed to initialize data writer: " << param_store.get<std::string>("pop_up_err_str");
+
+    // Will get data and write it at the same time
+    while (data_acq.get_batch_evt_data(evt_data, param_store, data_writer, 1.0f) ||
+           data_acq.get_batch_frame_data(evt_data, param_store, data_writer))
+    {
+    }
+
+    // Check order of timestamps, by aedat standard, should be ordered.
+    int32_t evt_ordered{check_order_events(evt_data)};
+    EXPECT_EQ(evt_ordered, -1) << "Events are out of order at: " << evt_ordered;
+    int32_t frame_ordered{check_order_frames(evt_data)};
+    EXPECT_EQ(frame_ordered, -1) << "Frames are out of order at: " << frame_ordered;
+
+    while (data_writer.write_frame_data(param_store) || data_writer.write_event_store(param_store))
+    {
+    }
+
+    // Clear data writer to flush io
+    data_writer.clear();
+
+    // Check that newly written file's data matches original file
+    DataAcquisition data_acq_out{};
+    EventData evt_data_out{};
+
+    // Initialize output file for reading
+    ASSERT_EQ(data_acq_out.init_file_reader("../testing/test_data_out.aedat4", param_store), true)
+        << "Failed to initialize file output for reading: " << param_store.get<std::string>("pop_up_err_str");
+
+    // Will get data and write it at the same time
+    while (data_acq_out.get_batch_evt_data(evt_data_out, param_store, data_writer, 1.0f) ||
+           data_acq_out.get_batch_frame_data(evt_data_out, param_store, data_writer))
+    {
+    }
+
+    // Check order of timestamps, by aedat standard, should be ordered.
+    int32_t evt_ordered_out{check_order_events(evt_data_out)};
+    EXPECT_EQ(evt_ordered_out, -1) << "Events are out of order at: " << evt_ordered_out;
+    int32_t frame_ordered_out{check_order_frames(evt_data_out)};
+    EXPECT_EQ(frame_ordered_out, -1) << "Frames are out of order at: " << frame_ordered_out;
+
+    // Check equality of written data with original data
+    int32_t evt_equality{check_event_data_equality(evt_data, evt_data_out)};
+    EXPECT_EQ(evt_equality, -1) << "Unequal events at: " << evt_equality;
+    int32_t frame_equality{check_frame_data_equality(evt_data, evt_data_out)};
+    EXPECT_EQ(frame_equality, -1) << "Unequal frames at: " << frame_equality;
 }
--- testing/unit_tests.cc
+++ testing/unit_tests.cc
@@ -1,24 +1,24 @@
-#include <gtest/gtest.h>
 #include "../src/EventData.hh"
 #include "../src/ParameterStore.hh"
+#include <gtest/gtest.h>
 #include <iostream>
 
-
+// Test settings and getting camera resolutions
 TEST(EventData, CameraResolution)
 {
     EventData test_ed{};
     test_ed.set_camera_event_resolution(1080, 1920);
     glm::vec2 evt_res{test_ed.get_camera_event_resolution()};
 
-    EXPECT_EQ(evt_res, glm::vec2(1080,1920)) << "Event Resolution Differs.";
+    EXPECT_EQ(evt_res, glm::vec2(1080, 1920)) << "Event Resolution Differs.";
 
     test_ed.set_camera_frame_resolution(1080, 1920);
     glm::vec2 frame_res{test_ed.get_camera_frame_resolution()};
 
-    EXPECT_EQ(evt_res, glm::vec2(1080,1920)) << "Frame Resolution Differs.";
-
+    EXPECT_EQ(evt_res, glm::vec2(1080, 1920)) << "Frame Resolution Differs.";
 }
 
+// Test writing event data
 TEST(EventData, write_evt_data)
 {
     EventData test_ed{};
@@ -26,7 +26,7 @@ TEST(EventData, write_evt_data)
     test_ed.set_camera_frame_resolution(1080, 1920);
 
     constexpr int32_t NUM_ELEMENTS{5};
-    for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+    for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
     {
         EventData::EventDatum evt_datum{.x = i, .y = i, .timestamp = i, .polarity = 0};
         test_ed.write_evt_data(evt_datum);
@@ -37,19 +37,18 @@ TEST(EventData, write_evt_data)
         test_ed.lock_data_vectors();
         const auto &evt_data_vec{test_ed.get_evt_vector_ref()};
         ASSERT_EQ(evt_data_vec.size(), NUM_ELEMENTS) << "Internal event data size mismatch.";
-        for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+        for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
         {
             glm::vec4 evt_datum{evt_data_vec[i]};
-            EXPECT_EQ(evt_datum, glm::vec4(i, i, i, 0)) << "Out of order writing of event data at " << i; 
+            EXPECT_EQ(evt_datum, glm::vec4(i, i, i, 0)) << "Out of order writing of event data at " << i;
         }
 
-        
         test_ed.unlock_data_vectors();
     }
 
     // Test event data timestamps are relative
     test_ed.clear();
-    for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+    for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
     {
         EventData::EventDatum evt_datum{.x = i, .y = i, .timestamp = i + 1, .polarity = 0};
         test_ed.write_evt_data(evt_datum);
@@ -57,18 +56,17 @@ TEST(EventData, write_evt_data)
     {
         test_ed.lock_data_vectors();
         const auto &evt_data_vec{test_ed.get_evt_vector_ref()};
-        ASSERT_EQ(evt_data_vec.size(), NUM_ELEMENTS) << "Internal event data size mismatch when testing relative timestamps.";
-        for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+        ASSERT_EQ(evt_data_vec.size(), NUM_ELEMENTS)
+            << "Internal event data size mismatch when testing relative timestamps.";
+        for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
         {
             glm::vec4 evt_datum{evt_data_vec[i]};
-            EXPECT_EQ(evt_datum, glm::vec4(i, i, i, 0)) << "Out of order writing of event data at " << i << " when testing relative timestamps."; 
+            EXPECT_EQ(evt_datum, glm::vec4(i, i, i, 0))
+                << "Out of order writing of event data at " << i << " when testing relative timestamps.";
         }
 
-        
         test_ed.unlock_data_vectors();
     }
-
-
 
     // Test camera reset
     EventData::EventDatum out_of_order_datum{.x = 0, .y = 0, .timestamp = 0, .polarity = 0};
@@ -76,12 +74,13 @@ TEST(EventData, write_evt_data)
     {
         test_ed.lock_data_vectors();
         const auto &evt_data_vec{test_ed.get_evt_vector_ref()};
-        EXPECT_EQ(evt_data_vec.size(), 1) << "Out of order writing of event data did not clear vector"; // Should have cleared vector before inserting
+        EXPECT_EQ(evt_data_vec.size(), 1)
+            << "Out of order writing of event data did not clear vector"; // Should have cleared vector before inserting
         test_ed.unlock_data_vectors();
     }
 
     // Write again after reset
-    for(int32_t i{1}; i < NUM_ELEMENTS; ++i)
+    for (int32_t i{1}; i < NUM_ELEMENTS; ++i)
     {
         EventData::EventDatum evt_datum{.x = i, .y = i, .timestamp = i, .polarity = 0};
         test_ed.write_evt_data(evt_datum);
@@ -91,19 +90,18 @@ TEST(EventData, write_evt_data)
         test_ed.lock_data_vectors();
         const auto &evt_data_vec{test_ed.get_evt_vector_ref()};
         ASSERT_EQ(evt_data_vec.size(), NUM_ELEMENTS) << "Internal event data size mismatch after reset.";
-        for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+        for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
         {
             glm::vec4 evt_datum{evt_data_vec[i]};
-            EXPECT_EQ(evt_datum, glm::vec4(i, i, i, 0)) << "Out of order writing of event data at " << i << " after reset."; 
+            EXPECT_EQ(evt_datum, glm::vec4(i, i, i, 0))
+                << "Out of order writing of event data at " << i << " after reset.";
         }
 
-        
         test_ed.unlock_data_vectors();
     }
-
 }
 
-
+// Test writing frame data
 TEST(EventData, write_frame_data)
 {
     EventData test_ed{};
@@ -124,7 +122,7 @@ TEST(EventData, write_frame_data)
     test_ed.write_evt_data(evt_datum);
 
     constexpr int32_t NUM_ELEMENTS{5};
-    for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+    for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
     {
         EventData::FrameDatum frame_datum{.frameData = cv::Mat(), .timestamp = i};
         test_ed.write_frame_data(frame_datum);
@@ -135,13 +133,12 @@ TEST(EventData, write_frame_data)
         test_ed.lock_data_vectors();
         const auto &frame_data_vec{test_ed.get_frame_vector_ref()};
         ASSERT_EQ(frame_data_vec.size(), NUM_ELEMENTS) << "Internal frame data size mismatch.";
-        for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+        for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
         {
             auto curr_timestamp{frame_data_vec[i].second};
-            EXPECT_EQ(curr_timestamp, i) << "Out of order writing of frame data at " << i; 
+            EXPECT_EQ(curr_timestamp, i) << "Out of order writing of frame data at " << i;
         }
 
-        
         test_ed.unlock_data_vectors();
     }
 
@@ -149,7 +146,7 @@ TEST(EventData, write_frame_data)
     test_ed.clear();
     EventData::EventDatum evt_datum_absolute{.x = 0, .y = 0, .timestamp = 1, .polarity = 0};
     test_ed.write_evt_data(evt_datum_absolute);
-    for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+    for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
     {
         EventData::FrameDatum frame_datum{.frameData = cv::Mat(), .timestamp = i + 1};
         test_ed.write_frame_data(frame_datum);
@@ -157,14 +154,15 @@ TEST(EventData, write_frame_data)
     {
         test_ed.lock_data_vectors();
         const auto &frame_data_vec{test_ed.get_frame_vector_ref()};
-        ASSERT_EQ(frame_data_vec.size(), NUM_ELEMENTS) << "Internal frame data size mismatch when testing relative timestamps.";
-        for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+        ASSERT_EQ(frame_data_vec.size(), NUM_ELEMENTS)
+            << "Internal frame data size mismatch when testing relative timestamps.";
+        for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
         {
             auto curr_timestamp{frame_data_vec[i].second};
-            EXPECT_EQ(curr_timestamp, i) << "Out of order writing of frame data at " << i << " when testing relative timestamps."; 
+            EXPECT_EQ(curr_timestamp, i) << "Out of order writing of frame data at " << i
+                                         << " when testing relative timestamps.";
         }
 
-        
         test_ed.unlock_data_vectors();
     }
 
@@ -172,16 +170,18 @@ TEST(EventData, write_frame_data)
     EventData::FrameDatum out_of_order_datum{.frameData = cv::Mat(), .timestamp = 0};
     test_ed.write_frame_data(out_of_order_datum); // Writing will fail since no event data exists after camera reset
     test_ed.write_evt_data(evt_datum);
-    test_ed.write_frame_data(out_of_order_datum); // Write again so write succeeds with timestamps relative to event data
+    test_ed.write_frame_data(
+        out_of_order_datum); // Write again so write succeeds with timestamps relative to event data
     {
         test_ed.lock_data_vectors();
         const auto &frame_data_vec{test_ed.get_frame_vector_ref()};
-        EXPECT_EQ(frame_data_vec.size(), 1) << "Out of order writing of event data did not clear vector"; // Should have cleared vector before inserting
+        EXPECT_EQ(frame_data_vec.size(), 1)
+            << "Out of order writing of event data did not clear vector"; // Should have cleared vector before inserting
         test_ed.unlock_data_vectors();
     }
 
     // Write again after reset
-    for(int32_t i{1}; i < NUM_ELEMENTS; ++i)
+    for (int32_t i{1}; i < NUM_ELEMENTS; ++i)
     {
         EventData::FrameDatum frame_datum{.frameData = cv::Mat(), .timestamp = i};
         test_ed.write_frame_data(frame_datum);
@@ -191,18 +191,17 @@ TEST(EventData, write_frame_data)
         test_ed.lock_data_vectors();
         const auto &frame_data_vec{test_ed.get_frame_vector_ref()};
         ASSERT_EQ(frame_data_vec.size(), NUM_ELEMENTS) << "Internal frame data size mismatch after reset.";
-        for(int32_t i{0}; i < NUM_ELEMENTS; ++i)
+        for (int32_t i{0}; i < NUM_ELEMENTS; ++i)
         {
             auto curr_timestamp{frame_data_vec[i].second};
-            EXPECT_EQ(curr_timestamp, i) << "Out of order writing of frame data at " << i << " after reset."; 
+            EXPECT_EQ(curr_timestamp, i) << "Out of order writing of frame data at " << i << " after reset.";
         }
 
-        
         test_ed.unlock_data_vectors();
     }
-
 }
 
+// Test getting earliest event timestamp
 TEST(EventData, get_earliest_evt_timestamp)
 {
     EventData test_ed{};
@@ -212,23 +211,25 @@ TEST(EventData, get_earliest_evt_timestamp)
 
     EventData::EventDatum evt_datum_order{.x = 0, .y = 0, .timestamp = 125, .polarity = 0};
     test_ed.write_evt_data(evt_datum_absolute);
-    EXPECT_EQ(test_ed.get_earliest_evt_timestamp(), 123) << "Earliest event timestamp mismatch after writing another datum.";
+    EXPECT_EQ(test_ed.get_earliest_evt_timestamp(), 123)
+        << "Earliest event timestamp mismatch after writing another datum.";
 
     EventData::EventDatum evt_datum_less{.x = 0, .y = 0, .timestamp = 3, .polarity = 0};
     test_ed.write_evt_data(evt_datum_less);
-    EXPECT_EQ(test_ed.get_earliest_evt_timestamp(), 3) << "Earliest event timestamp mismatch after out of order writing.";
-
+    EXPECT_EQ(test_ed.get_earliest_evt_timestamp(), 3)
+        << "Earliest event timestamp mismatch after out of order writing.";
 }
 
+// Testing getting index from timestamp
 TEST(EventData, get_event_index_from_timestamp)
 {
     EventData test_ed{};
     EventData::EventDatum evt_datum1{.x = 0, .y = 0, .timestamp = 0, .polarity = 0};
     test_ed.write_evt_data(evt_datum1);
-    
+
     {
         test_ed.lock_data_vectors();
-        
+
         int64_t bad_index1 = test_ed.get_event_index_from_relative_timestamp(2);
         EXPECT_EQ(bad_index1, -1) << "Expected -1 for non-existent timestamp.";
 
@@ -237,8 +238,7 @@ TEST(EventData, get_event_index_from_timestamp)
 
         int64_t good_index1 = test_ed.get_event_index_from_relative_timestamp(0);
         EXPECT_EQ(good_index1, 0) << "Expected 0 for existing timestamp.";
-        
-        
+
         test_ed.unlock_data_vectors();
     }
 
@@ -250,7 +250,7 @@ TEST(EventData, get_event_index_from_timestamp)
 
     {
         test_ed.lock_data_vectors();
-        
+
         int64_t good_index1 = test_ed.get_event_index_from_relative_timestamp(0);
         EXPECT_EQ(good_index1, 0) << "Expected 0 for existing timestamp.";
 
@@ -262,16 +262,16 @@ TEST(EventData, get_event_index_from_timestamp)
 
         int64_t good_index4 = test_ed.get_event_index_from_relative_timestamp(500);
         EXPECT_EQ(good_index4, 2) << "Expected 2 for timestamp greater than provided timestamp.";
-        
+
         int64_t bad_index1 = test_ed.get_event_index_from_relative_timestamp(10000);
-        EXPECT_EQ(bad_index1, -1) << "Expected -1 for non-existent timestamp (no timestamp greater than or equal to provided exist).";
+        EXPECT_EQ(bad_index1, -1)
+            << "Expected -1 for non-existent timestamp (no timestamp greater than or equal to provided exist).";
 
         test_ed.unlock_data_vectors();
     }
-
-
 }
 
+// Testing methods of ParameterStore
 TEST(ParameterStore, add_get_exists)
 {
     ParameterStore test_param_store{};
@@ -279,17 +279,16 @@ TEST(ParameterStore, add_get_exists)
     // Test basic adding and getting
     test_param_store.add("key1", 1);
     test_param_store.add("key2", std::string("value"));
-    
+
     EXPECT_EQ(test_param_store.get<int32_t>("key1"), 1) << "ParameterStore did not get correct int value.";
-    EXPECT_EQ(test_param_store.get<std::string>("key2"), std::string("value")) << "ParameterStore did not get correct string value.";
+    EXPECT_EQ(test_param_store.get<std::string>("key2"), std::string("value"))
+        << "ParameterStore did not get correct string value.";
 
     // Test modifying existing key
     test_param_store.add("key1", 123);
     EXPECT_EQ(test_param_store.get<int32_t>("key1"), 123) << "Parameter store did not correctly modify value.";
 
-
     // Test exist function
     EXPECT_EQ(test_param_store.exists("key1"), true) << "Parameter store did not detect existing key.";
     EXPECT_EQ(test_param_store.exists("abc"), false) << "Parameter store detected non-existent key.";
-
 }
